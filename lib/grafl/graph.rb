c5829eb07d708b2b371148af92d9babeed32fc78
Hayes Davis <hayes@appozite.com>
2010-11-29 18:44:04 -0800

    Added debugging output to Graph

--- lib/grafl/graph.rb
+++ lib/grafl/graph.rb
@@ -6,7 +6,7 @@ module Grafl
     
     include UriUtils
     
-    attr_accessor :access_token
+    attr_accessor :access_token, :debug
     
     def initialize(options={})
       self.access_token = options[:access_token]
@@ -21,11 +21,15 @@ module Grafl
       conn = Net::HTTP.new(uri.host,uri.port)
       conn.use_ssl = true
       conn.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      conn.start do |http|
+      res = conn.start do |http|
         req = request_class(method).new(uri.request_uri)
+        add_headers(req)
         add_form_data(req,params)
+        dump_request(req) if debug
         http.request(req)
       end
+      dump_response(res) if debug
+      res
     end
     
     def build_uri(method,path,params={})
@@ -37,6 +41,35 @@ module Grafl
     end
     
     private
+      def dump_request(req)
+        puts "Sending Request"
+        puts"#{req.method} #{req.path}"
+        dump_headers(req)
+        if req.body_exist?
+          puts
+          puts req.body
+        end
+        puts
+      end
+      
+      def dump_response(res)
+        puts "Response"
+        puts "#{res.code} #{res.message}"
+        dump_headers(res)
+        puts
+        puts res.body
+      end
+      
+      def dump_headers(msg)
+        msg.each_header do |key, value|
+          puts "#{key}=#{value}"
+        end
+      end
+      
+      def add_headers(req)
+        req["User-Agent"] = "graphl/#{Grafl::VERSION}"  
+      end
+      
       def request_class(method)
         Net::HTTP.const_get(method.to_s.capitalize)
       end
@@ -44,7 +77,7 @@ module Grafl
       def request_body_permitted?(req)
         req.request_body_permitted? || req.kind_of?(Net::HTTP::Delete)
       end
-
+      
       def add_form_data(req,params)
         if request_body_permitted?(req) && params
           req.set_form_data(params)
